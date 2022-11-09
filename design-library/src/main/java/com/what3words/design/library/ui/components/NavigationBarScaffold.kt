eb98f5060bee2758f78f44a44393b3efe7216e2a
Emanuel Amiguinho <emanuel@what3words.com>
2022-11-09 16:59:08 +0000

    Update NavigationBarScaffold.kt

--- design-library/src/main/java/com/what3words/design/library/ui/components/NavigationBarScaffold.kt
+++ design-library/src/main/java/com/what3words/design/library/ui/components/NavigationBarScaffold.kt
@@ -5,15 +5,17 @@ import androidx.compose.material.Scaffold
 import androidx.compose.material.Text
 import androidx.compose.material.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import com.what3words.design.library.ui.theme.W3WTheme
 
 @Composable
 fun NavigationBarScaffold(
+    modifier: Modifier = Modifier,
     title: String? = null,
     navigationIcon: (@Composable () -> Unit)? = null,
     bodyContent: @Composable (PaddingValues) -> Unit,
 ) {
-    Scaffold(topBar = {
+    Scaffold(modifier = modifier, topBar = {
         TopAppBar(
             backgroundColor = W3WTheme.colors.background,
             title = {
