d482ac96c32c5558b509c689548bd542c4afc000
Mani <emanuel@what3words.com>
2024-05-10 14:35:49 +0100

    Remove ResponsiveText.kt Added AutoSizeText Changed What3wordsAddress to have Secondary Text and Content Changed What3wordsAddressListItem to use What3wordsAddress with secondary content

---
+++ design-library/src/main/java/com/what3words/design/library/ui/components/ImmutableWrapper.kt
@@ -0,0 +1,11 @@
+package com.what3words.design.library.ui.components
+
+import androidx.compose.runtime.Immutable
+import kotlin.reflect.KProperty
+
+@Immutable
+data class ImmutableWrapper<T>(val value: T)
+
+fun <T> T.toImmutableWrapper() = ImmutableWrapper(this)
+
+operator fun <T> ImmutableWrapper<T>.getValue(thisRef: Any?, property: KProperty<*>) = value
