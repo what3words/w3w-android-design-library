a34a3c3e25fbfc83fa9b54f958b40089ebfc7b8a
Nghia Nguyen <nghia.nguyen@what3words.com>
2026-09-11 16:13:08 +0700

    nit

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -16,7 +16,7 @@ dependencyResolutionManagement {
     }
     versionCatalogs {
         create("libs") {
-            from("com.what3words:android-version-catalog:2026.10.01-SNAPSHOT")
+            from("com.what3words:android-version-catalog:2026.09.01")
 
             version("minSdk", "24")
         }
